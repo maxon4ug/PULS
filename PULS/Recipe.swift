38522ce0a68136a426d0a5432b8bde9f2f9b37cb
surgai <sketbordest@mail.ru>
2017-07-29 12:54:01 +0300

    Creating Recipe data model and implementing MVC

---
+++ PULS/Recipe.swift
@@ -0,0 +1,18 @@
+//
+//  Recipe.swift
+//  PULS
+//
+//  Created by Max Surgai on 29.07.17.
+//  Copyright © 2017 Max Surgai. All rights reserved.
+//
+
+import RealmSwift
+
+class Recipe: Object {
+    
+    dynamic var title = ""
+    dynamic var href = ""
+    dynamic var ingredients = ""
+    dynamic var thumbnail = ""
+    
+}
